b74cbf978c1c387412482eaea5dab15e331541b2
James Kinyua <jameskinyua590@gmail.com>
2017-06-12 15:54:44 +0300

    Add a test case test prime_nums_raises_ValueError_on_negative_input

--- test_prime_nums.py
+++ test_prime_nums.py
@@ -5,9 +5,22 @@ from prime_nums import generate_prime_nums
 class TestPrimeNumbers(unittest.TestCase):
     def setUp(self):
         self.prime_numbers = generate_prime_nums(10)
+        
     def test_prime_nums_is_a_list(self):
         self.assertTrue(type(self.prime_numbers) is list, 
         "generate_prime_numbers should return a list.")
+        
+    def test_prime_nums_produces_correct_output(self):
+        self.assertEqual(self.prime_numbers, [2, 3, 5, 7], 
+        "Should produce correct output")
+        
+    def test_prime_nums_raises_TypeError_on_string_input(self):
+        with self.assertRaises(TypeError):
+            generate_prime_nums('10')
+            
+    def test_prime_nums_raises_ValueError_on_negative_input(self):
+        with self.assertRaises(ValueError):
+            generate_prime_nums(-1)
 
 if __name__ == '__main__':
     unittest.main()
